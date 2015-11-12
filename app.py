a1671b027e833ffc698d19c51c1d03f55966197d
Anita Mehrotra <anita.mehrotra@buzzfeed.com>
2015-11-12 12:58:28 -0500

    updated to adhere to pep8; updated gitignore

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import numpy as np
-import sklearn
 from sklearn.cluster import AgglomerativeClustering
 
 
@@ -13,18 +12,18 @@ def cluster(number_of_clusters):
     :rtype: list of lists
     :return: clusters of hot guys' names
     """
-    matrix = np.array([[ 1.,  2.,  0.,  2.,  5.,  2.,  0.,  5.],
-    	[ 3.,  0.,  0.,  2.,  4.,  2.,  2.,  1.],
-    	[ 0.,  3.,  1.,  3.,  0.,  0.,  1.,  1.],
-    	[ 2.,  4.,  0.,  0.,  0.,  0.,  1.,  4.],
-    	[ 0.,  4.,  0.,  0.,  0.,  1.,  1.,  5.],
-    	[ 2.,  1.,  1.,  2.,  3.,  0.,  2.,  2.],
-    	[ 2.,  1.,  0.,  2.,  1.,  1.,  2.,  5.],
-    	[ 0.,  1.,  0.,  1.,  1.,  2.,  0.,  3.],
-    	[ 1.,  2.,  0.,  2.,  1.,  2.,  1.,  4.],
-    	[ 0.,  1.,  0.,  1.,  2.,  0.,  3.,  0.],
-    	[ 0.,  1.,  0.,  2.,  3.,  2.,  2.,  2.],
-    	[ 0.,  0.,  0.,  2.,  1.,  0.,  2.,  5.]])
+    matrix = np.array([[1.,  2.,  0.,  2.,  5.,  2.,  0.,  5.],
+                       [3.,  0.,  0.,  2.,  4.,  2.,  2.,  1.],
+                       [0.,  3.,  1.,  3.,  0.,  0.,  1.,  1.],
+                       [2.,  4.,  0.,  0.,  0.,  0.,  1.,  4.],
+                       [0.,  4.,  0.,  0.,  0.,  1.,  1.,  5.],
+                       [2.,  1.,  1.,  2.,  3.,  0.,  2.,  2.],
+                       [2.,  1.,  0.,  2.,  1.,  1.,  2.,  5.],
+                       [0.,  1.,  0.,  1.,  1.,  2.,  0.,  3.],
+                       [1.,  2.,  0.,  2.,  1.,  2.,  1.,  4.],
+                       [0.,  1.,  0.,  1.,  2.,  0.,  3.,  0.],
+                       [0.,  1.,  0.,  2.,  3.,  2.,  2.,  2.],
+                       [0.,  0.,  0.,  2.,  1.,  0.,  2.,  5.]])
     clustering_model = AgglomerativeClustering(n_clusters=number_of_clusters).fit(matrix)
     labels = clustering_model.labels_
     return str(labels)
@@ -35,7 +34,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return cluster(3)
+    return cluster(3)
 
 
 if __name__ == '__main__':
