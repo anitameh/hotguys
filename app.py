91791b1e7aca9810ce7b7932bd0190ae032a790d
Anita Mehrotra <anita.mehrotra@buzzfeed.com>
2015-11-12 17:15:11 -0500

    this app wont work rn, but it includes all functions that will make it work; some are scaffolded, others are complete

--- app.py
+++ app.py
@@ -3,7 +3,43 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 
 
-def cluster(data, number_of_clusters):
+def read_data_from_database(filename_database, filename_user_inputs):
+    """
+    Returns matrix with both reserved and user-selected hot guys
+    :param String filename_database:
+    :param String filename_user_inputs:
+
+    :rtype: triple
+    :return: matrix of values, list of urls for images, row indices of matrix corresponding to user_inputs
+    """
+    database = np.loadtxt(filename_database, dtype={'names': ('face_shape', 'skin_tone', 'hair_length', 'hair_type',
+                                                       'hair_color', 'lips', 'eye_color', 'nose_shape', 'url'),
+                                                    'formats': ('f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4',
+                                                                'S1000')},
+                          delimiter=',', skiprows=1, usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+    user_inputs = np.loadtxt(filename_user_inputs, dtype={'names': ('face_shape', 'skin_tone', 'hair_length',
+                                                                    'hair_type', 'hair_color', 'lips', 'eye_color',
+                                                                    'nose_shape', 'url'),
+                                                          'formats': ('f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4',
+                                                                      'S1000')},
+                             delimiter=',', skiprows=1, usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+    matrix, urls = [], []
+    for each_row in database:
+        matrix.append(list(each_row)[:-1])
+        urls.append(each_row[-1])
+
+    for each_row in user_inputs:
+        matrix.append(list(each_row)[:-1])
+        urls.append(each_row[-1])
+
+    indices_of_user_selected = range(len(database) + len(user_inputs))[-len(user_inputs):]
+
+    return matrix, urls, indices_of_user_selected
+
+
+def cluster(matrix, number_of_clusters):
     """
     Cluster hot guys by attributes.
     :param numpy.matrix matrix:
@@ -12,12 +48,21 @@ def cluster(data, number_of_clusters):
     :rtype: list of lists
     :return: clusters of hot guys' names
     """
-    matrix = []
-    for each_row in data:
-        matrix.append(list(each_row))
     clustering_model = AgglomerativeClustering(n_clusters=number_of_clusters).fit(matrix)
     labels = clustering_model.labels_
-    return str(labels)
+    return labels
+
+
+def recommend(urls, indices_of_user_selected):
+    """
+    Heuristics defined heeeeere.
+    :param list urls:
+    :param list indices_of_user_selected:
+
+    :rtype: String
+    :return: url pointing to image of recommended hot guy
+    """
+    # TODO
 
 
 app = Flask(__name__)
@@ -25,11 +70,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = np.loadtxt('database.csv', dtype={'names': ('face_shape', 'skin_tone', 'hair_length', 'hair_type',
-                                                       'hair_color', 'lips', 'eye_color', 'nose_shape'),
-                                             'formats': ('f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4')},
-                      delimiter=',', skiprows=1, usecols=(1, 2, 3, 4, 5, 6, 7, 8))
-    return cluster(data, 3)
+    matrix, urls, indices_of_user_selected = read_data_from_database('database.csv', 'user_inputs.csv')
+    clusters = str(cluster(matrix, 3))
+    return clusters
 
 if __name__ == '__main__':
     app.run(debug=True)
